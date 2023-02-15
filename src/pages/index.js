50b3245afceb46d2be4d9da7d1f82b7cda4d4121
EmilianoCBE <e.carrizales1305@gmail.com>
2023-02-15 14:06:59 -0600

    Extract Advantage section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Head from 'next/head'
 import { Header } from '@/components/Header'
 import { TopBar } from '@/components/TopBar'
 import { HomeCategories } from '@/components/HomeCategories'
+import { AdvantageSection } from '@/components/AdvantageSection'
 
-import { Box, Container, Flex } from '@chakra-ui/react'
-import { AdvantageItem } from '@/components/AdvantageItem'
-
+import { Box, Container } from '@chakra-ui/react'
 
 export default function Home({products, categories}) {
   return (
@@ -26,23 +25,7 @@ export default function Home({products, categories}) {
         <Container size="lg">
           <HomeCategories categories={categories}/>
 
-          <Flex justifyContent="space-between" margin="2rem 0">
-            <AdvantageItem 
-                title="Free shipping" 
-                content="On all UA order or order above $100"
-                icon="/icon-truck.svg"
-              />
-              <AdvantageItem 
-                title="30 days return" 
-                content="Simply return it within 30 days for an exchange"
-                icon="/icon-return.svg"
-              />
-              <AdvantageItem 
-                title="Support 24/7" 
-                content="Contact us 24 hours a day, 7 days a week"
-                icon="/icon-support.svg"
-              />
-          </Flex>
+          <AdvantageSection />
         </Container> 
         {/* {<ol>
           {products.map(product => {
